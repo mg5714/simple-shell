8954e0b8fc5b45cf99f1c835d4494c56ef87e250
Mohammad Fathi <mohfabdou@gmail.com>
2023-11-12 22:34:39 +0200

    added env and exit commands

---
+++ printenv.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+extern char **environ;
+/**
+ * print_env - print the current environment variables
+ * Return: void;
+ */
+void print_env()
+{
+	int i;
+
+	for (i = 0; environ[i] != NULL; i++)
+		printf("%s\n", environ[i]);
+
+	return;
+}
