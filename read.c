fb2b3a664dcfb771ca20b54aca8771ff442f20c8
Mohammad Fathi <mohfabdou@gmail.com>
2023-11-16 02:37:23 +0200

    fixed empty input through pipe

--- read.c
+++ read.c
@@ -1,5 +1,8 @@
 #include "shell.h"
 #include <stdlib.h>
+
+
+void rstrip(char *buffer);
 /**
  * read_line - reads a line from stdin
  * @buffer: buffer to store line
@@ -10,6 +13,7 @@
 void read_line(char *buffer, size_t buff_size, char **argv)
 {
 	ssize_t bytes;
+	char *lstrip;
 
 	if (isatty(fileno(stdin)))
 	{
@@ -29,11 +33,43 @@ void read_line(char *buffer, size_t buff_size, char **argv)
 		while ((bytes = getline(&buffer, &buff_size, stdin)) > 0)
 		{
 			remove_newline(buffer);
-			check_builtins(buffer);
-			execute_command(buffer, argv);
+			for (lstrip = buffer; *lstrip == ' '; lstrip++)
+				;
+			if (_strlen(lstrip) == 0)
+				break;
+
+			rstrip(lstrip);
+			check_builtins(lstrip);
+			execute_command(lstrip, argv);
 		}
 		exit(0);
 
 	}
 
+}
+
+/**
+ * rstrip - delete whitespaces in the right side of the string
+ * @buffer: pointer to the buffer used
+ * Return: void
+ */
+void rstrip(char *buffer)
+{
+	int i, len;
+
+	len = _strlen(buffer);
+	if (len < 1)
+		return;
+
+	if (buffer[len - 2] != ' ')
+		return;
+
+	for (i = _strlen(buffer) - 2; i >= 0; i--)
+	{
+		if (*(buffer + i) != ' ')
+		{
+			*(buffer + i + 1) = '\0';
+			return;
+		}
+	}
 }
